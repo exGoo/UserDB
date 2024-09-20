0f04d7ccb3b8d4d42c1ba1dab2425b404702c613
Vladislav Guliaev <nascentesxxx@gmail.com>
2024-09-20 05:46:53 +0400

    Шаблон для задачи с Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,23 +2,14 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import org.hibernate.engine.config.internal.ConfigurationServiceImpl;
 
+import java.lang.module.Configuration;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserServiceImpl service = new UserServiceImpl();
-        service.createUsersTable();
-
-        service.saveUser("Влад", "Гуляев", (byte) 24);
-        service.saveUser("Иван", "Иванов", (byte) 29);
-        service.saveUser("Антон", "Антонов", (byte) 26);
-        service.saveUser("Алексей", "Алексеев", (byte) 24);
-
-        List<User> list = service.getAllUsers();
-        service.cleanUsersTable();
-        service.dropUsersTable();
     }
 
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,22 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.Session;
+import com.mysql.cj.xdevapi.SessionFactory;
+
+import java.lang.module.Configuration;
 import java.sql.*;
 
 public class Util {
+    private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
-        Connection connection;
-        final String NAME_USER  = "root";
-        final String NAME_PASS = "---VLAD---xXx---2000";
-        final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-        try {
-            connection = DriverManager.getConnection(URL, NAME_USER, NAME_PASS);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        return connection;
     }
-
 }
